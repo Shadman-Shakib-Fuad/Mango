df59c5f1aa101fe38dd8f1f7664101ceb2f3a8e8
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-02 22:55:31 +0600

    Footer part added

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer className="footer p-10 bg-base-200">
+      <div>
+        <h2 className="font-bold">Mango Library</h2>
+        <p>Contact: mango@gmail.com</p>
+      </div>
+    </footer>
+  );
+}
