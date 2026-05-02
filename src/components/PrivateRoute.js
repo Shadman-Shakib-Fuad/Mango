73f3311214ea56224e498b702a90ff572eece2b7
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-02 23:20:37 +0600

    Private Route

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+"use client";
+import { useAuth } from "../context/AuthContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function PrivateRoute({ children }) {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [user]);
+
+  if (!user) return null;
+
+  return children;
+}
