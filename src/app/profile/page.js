b2260a17dce26e5c8167f728166ead4796b3cbb4
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-02 23:00:50 +0600

    Profile Page

---
+++ src/app/profile/page.js
@@ -0,0 +1,8 @@
+export default function Profile() {
+  return (
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">My Profile</h1>
+      <button className="btn mt-4">Update Info</button>
+    </div>
+  );
+}
