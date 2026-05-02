1442be3a3efe0ed8d5e61be55865734028e66b99
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-03 00:18:55 +0600

    AUTH CONFIG

---
+++ src/lib/auth.js
@@ -0,0 +1,30 @@
+import GoogleProvider from "next-auth/providers/google";
+import clientPromise from "./db";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "test",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "test",
+    }),
+  ],
+
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async signIn({ user }) {
+      const client = await clientPromise;
+      const db = client.db("mango");
+
+      const exist = await db.collection("users").findOne({
+        email: user.email,
+      });
+
+      if (!exist) {
+        await db.collection("users").insertOne(user);
+      }
+
+      return true;
+    },
+  },
+};
