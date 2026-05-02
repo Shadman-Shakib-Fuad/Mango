b725061ca9ffa18b234a2b9046924334e333f341
Shadman-Shakib-Fuad <shadmanfuad456@gmail.com>
2026-05-02 23:21:21 +0600

    Profile Page Protected

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,8 +1,21 @@
+"use client";
+
+import PrivateRoute from "../../components/PrivateRoute";
+import { useAuth } from "../../context/AuthContext";
+
 export default function Profile() {
+  const { user } = useAuth();
+
   return (
-    <div className="p-10">
-      <h1 className="text-2xl font-bold">My Profile</h1>
-      <button className="btn mt-4">Update Info</button>
-    </div>
+    <PrivateRoute>
+      <div className="p-10">
+        <h1 className="text-2xl font-bold">My Profile</h1>
+
+        <div className="mt-4 space-y-2">
+          <p><b>Name:</b> {user?.name}</p>
+          <p><b>Email:</b> {user?.email}</p>
+        </div>
+      </div>
+    </PrivateRoute>
   );
 }
